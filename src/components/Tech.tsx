8f9ee751ae42a3d95135aa6d5e79305fad4ef1d7
Bryan Grill <grillb@gmail.com>
2019-11-06 06:02:19 -0500

    start add grid

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -5,16 +5,49 @@ const Tech = () => {
   return (
     <Box
       sx={{
-        py: 4,
+        py: [2, 3, 4],
       }}
     >
       <Heading
         sx={{
           fontSize: [3, 4, 5],
+          mb: 3,
         }}
       >
         Tech I Use
       </Heading>
+      <Box
+        sx={{
+          display: 'grid',
+          gridGap: 4,
+          gridTemplateColumns: 'repeat(auto-fit, minmax(128px, 1fr))',
+        }}
+      >
+        <Heading p={3} bg="muted">
+          Hello
+        </Heading>
+        <Box p={3} color="background" bg="primary">
+          CSS Grid
+        </Box>
+        <Heading p={3} bg="muted">
+          Hello
+        </Heading>
+        <Box p={3} color="background" bg="primary">
+          CSS Grid
+        </Box>
+        <Heading p={3} bg="muted">
+          Hello
+        </Heading>
+        <Box p={3} color="background" bg="primary">
+          CSS Grid
+        </Box>
+        <Heading p={3} bg="muted">
+          Hello
+        </Heading>
+        <Box p={3} color="background" bg="primary">
+          CSS Grid
+        </Box>
+      </Box>
     </Box>
   );
 };
