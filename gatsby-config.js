26236b481843b36b1b4933bb6fa223f329765838
Bryan Grill <grillb@gmail.com>
2019-11-08 15:50:55 -0500

    add back plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,25 @@ module.exports = {
     {
       resolve: 'gatsby-transformer-remark',
       options: {
-        plugins: [],
+        plugins: [
+          {
+            resolve: 'gatsby-remark-responsive-iframe',
+            options: {
+              wrapperStyle: 'margin-bottom: 1rem',
+            },
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1140,
+              quality: 90,
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
       },
     },
     'gatsby-transformer-json',
