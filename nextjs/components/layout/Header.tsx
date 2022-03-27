1afd8b3877bba7611d97835134aaf588f39ce9ad
Bryan Grill <grillb@gmail.com>
2022-03-27 15:14:16 -0400

    add nav menu

--- nextjs/components/layout/Header.tsx
+++ nextjs/components/layout/Header.tsx
@@ -1,6 +1,41 @@
-/** Render `nav` component across app */
-const Header = () => {
-  return <nav>header nav here</nav>;
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const links = [
+  { href: '/', label: 'Home', key: 'home' },
+  { href: '/projects', label: 'Projects', key: 'projects' },
+  { href: '/about', label: 'About', key: 'about' },
+];
+
+const NavMenu = () => {
+  const { route } = useRouter();
+  return (
+    <nav>
+      <ul className="flex">
+        {links.map(({ href, label, key }) => (
+          <li key={key} className="mr-2 md:mr-8">
+            <Link href={href}>
+              <a
+                className={`${
+                  route === href
+                    ? 'font-semibold text-cyan-400 hover:text-cyan-400'
+                    : 'font-normal hover:text-gray-400'
+                }`}
+              >
+                {label}
+              </a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
-export default Header;
+/** Render `header` and `nav` elements across app */
+export const Header = () => (
+  <header className="flex justify-between items-center p-2 md:p-4">
+    <NavMenu />
+    <div>links here</div>
+  </header>
+);
