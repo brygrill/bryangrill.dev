5b32067afaacd2fffadfc9d2db7315953c14696e
Bryan Grill <grillb@gmail.com>
2022-03-27 13:40:12 -0400

    add banner component

--- nextjs/components/Banner.tsx
+++ nextjs/components/Banner.tsx
@@ -1,12 +1,19 @@
 import Image from 'next/image';
+import bannerImg from '../public/images/banner.svg';
 
 /** Render Hero image and Banner text */
 const Banner = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Banner image here</h1>{' '}
+    <div className="flex flex-col md:flex-row items-center px-4 py-8">
+      <span className="p-2 md:p-0 md:pr-2">
+        <h1 className="text-6xl font-bold">Hi!</h1>
+        <h3 className="text-2xl">
+          {`I'm Bryan and I'm software developer. I work on the frontend, backend,
+        and in the cloud.`}
+        </h3>
+      </span>
       <span>
-        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        <Image src={bannerImg} alt="Hero image" />
       </span>
     </div>
   );
