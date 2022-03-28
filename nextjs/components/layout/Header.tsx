b5ea5a2e8259bb5b446fa7a7caa68e35746fc185
Bryan Grill <grillb@gmail.com>
2022-03-27 21:38:01 -0400

    add external links header

--- nextjs/components/layout/Header.tsx
+++ nextjs/components/layout/Header.tsx
@@ -1,12 +1,35 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
-import bannerImg from '../../public/images/linkedin-brands.svg';
+import linkedinImg from '../../public/images/links/linkedin.svg';
+import githubImg from '../../public/images/links/github.svg';
+import resumeImg from '../../public/images/links/represent-io.svg';
 
-const links = [
+const nav = [
   { href: '/', label: 'Home', key: 'home' },
   { href: '/projects', label: 'Projects', key: 'projects' },
   { href: '/about', label: 'About', key: 'about' },
+];
+
+const links = [
+  {
+    href: 'https://github.com/brygrill',
+    label: 'Github',
+    key: 'github',
+    src: githubImg,
+  },
+  {
+    href: 'https://www.linkedin.com/in/bryangrill',
+    label: 'LinkedIn',
+    key: 'linkedin',
+    src: linkedinImg,
+  },
+  {
+    href: 'https://represent.io/bryangrill.pdf',
+    label: 'Resume',
+    key: 'resume',
+    src: resumeImg,
+  },
 ];
 
 const NavMenu = () => {
@@ -14,8 +37,8 @@ const NavMenu = () => {
   return (
     <nav>
       <ul className="flex">
-        {links.map(({ href, label, key }) => (
-          <li key={key} className="mr-2 md:mr-8">
+        {nav.map(({ href, label, key }) => (
+          <li key={key} className="mr-4 md:mr-8">
             <Link href={href}>
               <a
                 className={`${
@@ -34,14 +57,26 @@ const NavMenu = () => {
   );
 };
 
+const ExternalLinks = () => (
+  <div>
+    {links.map(({ href, label, key, src }) => (
+      <a
+        href={href}
+        key={key}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="ml-3 md:ml-4"
+      >
+        <Image src={src} alt={label} height={32} width={32} />
+      </a>
+    ))}
+  </div>
+);
+
 /** Render `header` and `nav` elements across app */
 export const Header = () => (
   <header className="flex justify-between items-center p-2 md:p-4">
     <NavMenu />
-    <div>
-      <a href="https://google.com">
-        <Image src={bannerImg} alt="Hero image" height={24} width={24} />
-      </a>
-    </div>
+    <ExternalLinks />
   </header>
 );
