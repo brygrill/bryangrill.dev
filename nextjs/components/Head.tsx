cecae1a4f2721d1f6f0a437453be7bff714a5de1
Bryan Grill <grillb@gmail.com>
2022-03-29 18:21:55 -0400

    update tags

--- nextjs/components/Head.tsx
+++ nextjs/components/Head.tsx
@@ -5,11 +5,36 @@ export const Head = () => {
   return (
     <NextHead>
       <title>Bryan Grill</title>
+      <link rel="icon" href="/favicon.png" />
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+      />
       <meta
         name="description"
-        content="Software Developer | TypeScript | React | NodeJS"
+        content="Personal website of software developer Bryan Grill. He enjoys working with React, Next.js, and TypeScript."
       />
-      <link rel="icon" href="/favicon.png" />
+      <meta property="og:title" content="Bryan Grill - Software Developer" />
+      <meta
+        property="og:description"
+        content="Bryan Grill | Sofware Developer | React | NodeJS | TypeScript"
+      />
+      <meta
+        property="og:image"
+        content="https://bryangrill.dev/_next/static/media/banner.05498a78.svg"
+      />
+      <meta property="og:url" content="https://bryangrill.dev" />
+      <meta
+        name="twitter:card"
+        content="Personal website of software developer Bryan Grill"
+      />
+      <meta
+        property="og:site_name"
+        content="Personal website of software developer Bryan Grill"
+      />
+      <meta name="twitter:image:alt" content="Banner image" />
     </NextHead>
   );
 };
